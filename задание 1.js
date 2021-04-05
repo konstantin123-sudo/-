916b6d902c41c88e3fd1ebf058aaa0bd8e4af286
konstantin123-sudo <81141905+konstantin123-sudo@users.noreply.github.com>
2021-04-05 17:38:51 +0300

    Add files via upload

---
+++ задание 1.js
@@ -0,0 +1,6 @@
+var a = 2;
+var b = 18;
+while(a<b){
+a++;
+if(a%2) alert(a);
+}
---
+++ задание 2.js
@@ -0,0 +1,7 @@
+for (var h = 0; h < 8; h++) {
+    for (var w = 0; w < h+1; w++) {
+    document.write("*\n");
+    }
+    document.write("<br>");
+    }
+    document.write("<hr>");
